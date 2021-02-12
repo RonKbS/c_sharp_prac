d8fef6482b345977481c224902728a2d0e768311
RonKbS <stephen.bamwite@gmail.com>
2021-02-12 18:16:29 +0300

    input digits with exception

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace c_sharp_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var incorrectValue = true;
+            while (incorrectValue)
+            {
+                try
+                {
+                    Console.Write("\nInput the First Number: ");
+                    var number1 = int.Parse(Console.ReadLine());
+                    Console.Write("\nInput the Second Number: ");
+                    var number2 = int.Parse(Console.ReadLine());
+                    var temp = number1;
+                    number1 = number2;
+                    number2 = temp;
+                    Console.Write($"\nAfter Swapping, first number is {number1} and second number is  {number2}");
+                    incorrectValue = false;
+                }
+                catch (System.FormatException)
+                {
+                    Console.Write("\nPlease input numerical values only");
+                }
+            }
+        }
+    }
+}
